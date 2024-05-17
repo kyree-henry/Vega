ce3771d5c7c747bcbaa86d505019ad8ac93c99c1
Kyree henry <ene.henry.eh@gmail.com>
2024-05-18 00:48:38 +0100

    Add project files.

---
+++ src/API/Models/Make.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models
+{
+    public class Make
+    {
+        public Make()
+        {
+            Models = [];
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public ICollection<Model> Models { get; set; }
+    }
+}
---
+++ src/API/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public int MakeId { get; set; }
+        public Make? Make { get; set; }
+    }
+}
