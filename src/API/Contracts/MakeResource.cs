3081cb975e1018a0b3df65a720f8e985a7cc13b7
Kyree henry <ene.henry.eh@gmail.com>
2024-05-20 19:12:26 +0100

    added contracts resources

---
+++ src/API/Contracts/MakeResource.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public class MakeResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public IEnumerable<ModelResource>? Models { get; set; }
+    }
+}
---
+++ src/API/Contracts/ModelResource.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.Contracts
+{
+    public class ModelResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        public int MakeId { get; set; }
+    }
+}
