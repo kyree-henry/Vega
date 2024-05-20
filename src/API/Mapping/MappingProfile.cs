efa06e3a881d99ae44e7cfcb286ee124379e435d
Kyree henry <ene.henry.eh@gmail.com>
2024-05-20 19:13:08 +0100

    added auto mapper and mapping profile

---
+++ src/API/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using API.Contracts;
+using API.Models;
+using AutoMapper;
+
+namespace API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<MakeResource, Make>().ReverseMap().ForMember(dest => dest.Models, opt => opt.MapFrom(src => src.Models));
+        }
+    }
+}
